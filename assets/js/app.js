1b9dc53a780f9d637074e8888003b7e5c4878271
Maggie29 <zmagali@yahoo.com>
2017-03-02 17:57:06 -0500

    actualizacio

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var formulario = document.getElementById("enviarFormulario")
+formulario.onclick = function(){
+  var nombre=document.getElementById("name").value;
+  var apellido = document.getElementById("lastName").value;
+  var numDni = document.getElementById("dni").value;
+  var direccion = document.getElementById("address").value
+  var lista =" <ul><li>NOMBRE: "+nombre+"</li>"+
+                "<li>APELLIDO: "+apellido+"</li>"+
+                "<li>DNI: "+numDni+"</li>"+
+                "<li>DIRECCION: "+direccion+"</li>"+
+                "</ul>"
+                result.innerHTML = lista;
+}
